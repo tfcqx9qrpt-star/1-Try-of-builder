0acc0fddb13d2ce921a153998061625f009c3b99
tfcqx9qrpt-star <tfcqx9qrpt@privaterelay.appleid.com>
2026-05-23 00:57:30 +0200

    Home, Beam1

--- Assets/scripts/Mausclick.cs
+++ Assets/scripts/Mausclick.cs
@@ -1,22 +1,32 @@
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using static Unity.Collections.AllocatorManager;
 
 
 public class Mausclick : MonoBehaviour
 {
+    private GameObject previewBlock;
+    private GameObject selectedPrefab;
+    private bool isBuildingBeam = false;
+    private Vector3 beamStartPos;
+    private GameObject beamPreview;
+    private bool beamMode = false;
+
+    public GameObject beamPrefab;
     public GameObject squarePrefab;
     public GameObject circlePrefab;
     public GameObject trianglePrefab;
-
-    private GameObject selectedPrefab;
     public float gridSize = 0.25f;
+    public float maxBeamLength = 5f;
     public List<GameObject> placedBlocks = new List<GameObject>();
     public GameObject previewPrefabS;
     public GameObject previewPrefabC;
     public GameObject previewPrefabT;
-    private GameObject previewBlock;
+    public GameObject previewPrefab1B;
+    public GameObject previewPrefab2B;
+
     Collider2D hit;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -33,6 +43,7 @@ public class Mausclick : MonoBehaviour
             selectedPrefab = squarePrefab;
             Destroy(previewBlock);
             previewBlock = Instantiate(previewPrefabS);
+            beamMode = false;
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2))
@@ -40,6 +51,7 @@ public class Mausclick : MonoBehaviour
             selectedPrefab = circlePrefab;
             Destroy(previewBlock);
             previewBlock = Instantiate(previewPrefabC);
+            beamMode = false;
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha3))
@@ -47,6 +59,14 @@ public class Mausclick : MonoBehaviour
             selectedPrefab = trianglePrefab;
             Destroy(previewBlock);
             previewBlock = Instantiate(previewPrefabT);
+            beamMode = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Destroy(previewBlock);
+            previewBlock = Instantiate(previewPrefab1B);
+            beamMode = true;
         }
 
         Vector3 mousePos = Input.mousePosition;
@@ -55,16 +75,63 @@ public class Mausclick : MonoBehaviour
         worldPos.x = Mathf.Round(worldPos.x / gridSize) * gridSize;
         worldPos.y = Mathf.Round(worldPos.y / gridSize) * gridSize;
         worldPos.z = 0f;
-        previewBlock.transform.position = worldPos;
+        if (!isBuildingBeam)
+        {
+            previewBlock.transform.position = worldPos;
+        }
+
+        if (isBuildingBeam)
+        {
+            Vector3 direction = worldPos - beamStartPos;
+
+            if (direction.magnitude > maxBeamLength)
+            {
+                direction = direction.normalized * maxBeamLength;
+            }
+
+            Vector3 limitedEndPos = beamStartPos + direction;
+
+            UpdateBeamVisual(beamPreview, beamStartPos, limitedEndPos);
+        }
 
         if (Input.GetMouseButtonDown(0))
         {
+            if (beamMode)
+            {
+                if (!isBuildingBeam)
+                {
+                    beamStartPos = worldPos;
+                    isBuildingBeam = true;
+                    beamPreview = Instantiate(previewPrefab2B, beamStartPos, Quaternion.identity);
+                }
+                else
+                {
+                    Vector3 direction = worldPos - beamStartPos;
 
-            Debug.Log(worldPos);
-          
-            //erstellte prefabs gehen direkt in die liste
-            GameObject newBlock = Instantiate(selectedPrefab, worldPos, Quaternion.identity);
-            placedBlocks.Add(newBlock);
+                    if (direction.magnitude > maxBeamLength)
+                    {
+                        direction = direction.normalized * maxBeamLength;
+                    }
+
+                    Vector3 limitedEndPos = beamStartPos + direction;
+
+                    GameObject newBeam = Instantiate(beamPrefab);
+                    UpdateBeamVisual(newBeam, beamStartPos, limitedEndPos);
+
+                    Destroy(beamPreview);
+                    beamPreview = null;
+                    isBuildingBeam = false;
+                }
+            }
+            else
+            {
+                Debug.Log(worldPos);
+
+                //erstellte prefabs gehen direkt in die liste
+                GameObject newBlock = Instantiate(selectedPrefab, worldPos, Quaternion.identity);
+                placedBlocks.Add(newBlock);
+            }
+            
 
         }
         if (Input.GetKeyDown(KeyCode.D))
@@ -100,5 +167,14 @@ public class Mausclick : MonoBehaviour
                
             }
         }
+    }
+    void UpdateBeamVisual(GameObject beam, Vector3 start, Vector3 end)
+    {
+        Vector3 middle = (start + end) / 2f;
+        Vector3 direction = end - start;
+
+        beam.transform.position = middle;
+        beam.transform.right = direction;
+        beam.transform.localScale = new Vector3(direction.magnitude, 0.2f, 1f);
     }
 }
